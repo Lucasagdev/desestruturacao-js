bfb8e4c51247dcc54a0de5b283b9738af3b270d7
Lucas Gomes <lucasagostini400@gmail.com>
2024-09-28 16:43:08 -0300

    Aprendendo desestruturar arrats no Javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+// let pessoa = { nome: 'Lucas', sobrenome: 'Gomes', idade: 33 }
+// let nome = pessoa.nome
+// let idade = pessoa.idade
+
+// console.log(nome)
+// console.log(idade)
+
+// let { nome: nome, idade: idade, sobrenome: sobrenome } = pessoa
+
+// console.log(nome)
+// console.log(sobrenome)
+// console.log(idade)
+
+const numeros = [1, 2, 3];
+
+const [ um, dois, tres ] = numeros
+
+console.log(um)
+console.log(dois)
+console.log(tres)
